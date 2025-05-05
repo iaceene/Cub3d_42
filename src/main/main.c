585544c45a7feb2afcc878e0b65c06d411726990
iaceene <yassineajagrou986@gmail.com>
2025-05-05 02:03:59 +0100

    init the parsing

--- src/main/main.c
+++ src/main/main.c
@@ -15,6 +15,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdbool.h>
+#include <strings.h>
+#include <string.h>
 #include <sys/time.h>
 #include <time.h>
 #include <math.h>
@@ -22,10 +24,10 @@
 #include <X11/X.h>
 #include "../../mlx/mlx.h"
 
-#define MOVE_SPEED 1
+#define MOVE_SPEED 4
 #define TILE_SIZE 64
-#define WINDOW_WIDTH (TILE_SIZE * 20)
-#define WINDOW_HEIGHT (TILE_SIZE * 20)
+#define WINDOW_WIDTH (TILE_SIZE * 10)
+#define WINDOW_HEIGHT (TILE_SIZE * 10)
 
 
 typedef struct s_img
@@ -45,12 +47,19 @@ typedef struct s_point
     int     pos_pix_y;
 } t_point;
 
+typedef struct s_wall
+{
+    int     x;
+    int     y;
+} t_wall;
+
 typedef struct s_list
 {
     void *win;
     void *mlx;
     t_img *img;
     t_point p;
+    t_wall *wall;
 } t_list;
 
 void my_pixel_put(int x, int y, t_img *img, int color)
@@ -98,8 +107,53 @@ void    draw_player(t_list *data)
     int x = data->p.x;
     int y = data->p.y;
 
+    int i = 0;
+
 
     my_pixel_put(x * TILE_SIZE + data->p.pos_pix_x, y * TILE_SIZE + data->p.pos_pix_y, data->img, 0);
+    
+    while (i < 3)
+    {
+        my_pixel_put(x * TILE_SIZE + data->p.pos_pix_x + i, y * TILE_SIZE + data->p.pos_pix_y, data->img, 0);
+        my_pixel_put(x * TILE_SIZE + data->p.pos_pix_x - i, y * TILE_SIZE + data->p.pos_pix_y, data->img, 0);
+        my_pixel_put(x * TILE_SIZE + data->p.pos_pix_x, y * TILE_SIZE + data->p.pos_pix_y + i, data->img, 0);
+        my_pixel_put(x * TILE_SIZE + data->p.pos_pix_x, y * TILE_SIZE + data->p.pos_pix_y - i, data->img, 0);
+        i++;
+    }
+    mlx_put_image_to_window(data->mlx, data->win, data->img->img, 0, 0);
+}
+
+
+int is_wall(int x, int y, t_list *data)
+{
+    int i = 0;
+    while (i < (WINDOW_HEIGHT * WINDOW_WIDTH))
+    {
+        if (data->wall[i].x == x && data->wall[i].y == y)
+            return(1);
+        i++;
+    }
+    return (0);
+}
+
+void    clear_player(t_list *data)
+{
+    int x = data->p.x;
+    int y = data->p.y;
+
+    int i = 0;
+
+
+    my_pixel_put(x * TILE_SIZE + data->p.pos_pix_x, y * TILE_SIZE + data->p.pos_pix_y, data->img, 0xFFFFFF);
+    
+    while (i < 3)
+    {
+        my_pixel_put(x * TILE_SIZE + data->p.pos_pix_x + i, y * TILE_SIZE + data->p.pos_pix_y, data->img, 0xFFFFFF);
+        my_pixel_put(x * TILE_SIZE + data->p.pos_pix_x - i, y * TILE_SIZE + data->p.pos_pix_y, data->img, 0xFFFFFF);
+        my_pixel_put(x * TILE_SIZE + data->p.pos_pix_x, y * TILE_SIZE + data->p.pos_pix_y + i, data->img, 0xFFFFFF);
+        my_pixel_put(x * TILE_SIZE + data->p.pos_pix_x, y * TILE_SIZE + data->p.pos_pix_y - i, data->img, 0xFFFFFF);
+        i++;
+    }
     mlx_put_image_to_window(data->mlx, data->win, data->img->img, 0, 0);
 }
 
@@ -114,10 +168,7 @@ void fill_background(t_list *data)
         x = 0;
         while (x < WINDOW_WIDTH / TILE_SIZE)
         {
-            if (data->p.x != x || data->p.y != y)
-                fill_tile(data->img, 0xFFFFFF, y, x);
-            else
-                fill_tile(data->img, 0x00FFFF, y, x);
+            fill_tile(data->img, 0xFFFFFF, y, x);
             x++;
         }
         y++;
@@ -140,21 +191,90 @@ int close_window(void *param)
     exit(0);
 }
 
+void move_down(t_list *data)
+{
+    if (is_wall(data->p.pos_pix_x, data->p.pos_pix_y + 1, data))
+        return ;
+    clear_player(data);
+    data->p.pos_pix_y += MOVE_SPEED;
+    draw_player(data);
+}
+
+void move_up(t_list *data)
+{
+    if (is_wall(data->p.pos_pix_x, data->p.pos_pix_y - 1, data))
+        return ;
+    clear_player(data);
+    data->p.pos_pix_y -= MOVE_SPEED;
+    draw_player(data);
+}
+
+void move_right(t_list *data)
+{
+    if (is_wall(data->p.pos_pix_x + 1, data->p.pos_pix_y, data))
+        return ;
+    clear_player(data);
+    data->p.pos_pix_x += MOVE_SPEED;
+    draw_player(data);
+}
+
+void move_left(t_list *data)
+{
+    if (is_wall(data->p.pos_pix_x - 1, data->p.pos_pix_y, data))
+        return ;
+    clear_player(data);
+    data->p.pos_pix_x -= MOVE_SPEED;
+    draw_player(data);
+}
+
 int key_bind(int key, void *param)
 {
     t_list *data = (t_list *)param;
 
     if (key == XK_Escape)
         close_window(data);
-	// if (key == XK_w)
-	// 	move_up(data);
-	// if (key == XK_s)
-	// 	move_down(data);
-	// if (key == XK_d)
-	// 	move_right(data);
-	// if (key == XK_a)
-	// 	move_left(data);
+	if (key == XK_w)
+		move_up(data);
+	if (key == XK_s)
+		move_down(data);
+	if (key == XK_d)
+		move_right(data);
+	if (key == XK_a)
+		move_left(data);
     return (0);
+}
+
+void draw_walls(t_list *data)
+{
+    int y;
+    int x;
+
+    y = 0;
+    x = 0;
+    int i = 0;
+    while (y < WINDOW_HEIGHT)
+    {
+        x = 0;
+        while (x < WINDOW_WIDTH)
+        {
+            if ((y >= 0 && y <= 3)
+            || (x >= 0 && x <= 3)
+            || (y + 3 == WINDOW_HEIGHT)
+            || (y + 2 == WINDOW_HEIGHT)
+            || (y + 1 == WINDOW_HEIGHT)
+            || (x + 3 == WINDOW_WIDTH)
+            || (x + 2 == WINDOW_WIDTH)
+            || (x + 1 == WINDOW_WIDTH))
+            {
+                my_pixel_put(x, y, data->img, 0x00FF00);
+                data->wall[i].x = x;
+                data->wall[i].y = y;
+                i++;
+            }
+            x++;
+        }
+        y++;
+    }
 }
 
 int main()
@@ -168,7 +288,12 @@ int main()
     data.p.y = 0;
     data.p.pos_pix_x = 32;
     data.p.pos_pix_y = 32;
+    data.wall = malloc(sizeof(t_wall) * WINDOW_HEIGHT * WINDOW_WIDTH);
+    if (!data.wall)
+        return (1);
+    bzero(data.wall, sizeof(t_wall) * WINDOW_HEIGHT * WINDOW_WIDTH);
     fill_background(&data);
+    draw_walls(&data);
     draw_player(&data);
     mlx_hook(data.win, 17, 0, close_window, &data);
     mlx_hook(data.win, 2, 1L<<0, key_bind, &data);
