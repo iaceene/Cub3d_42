dfba58c04210401315a3a21e84e20402aed0eed9
iaceene <yassineajagrou986@gmail.com>
2025-06-18 22:25:44 +0100

    fixing parse

--- src/components/parsing/map_parsing_utile_6.c
+++ src/components/parsing/map_parsing_utile_6.c
@@ -6,7 +6,7 @@
 /*   By: yaajagro <yaajagro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 22:15:13 by yaajagro          #+#    #+#             */
-/*   Updated: 2025/06/18 22:19:40 by yaajagro         ###   ########.fr       */
+/*   Updated: 2025/06/18 22:25:25 by yaajagro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -103,7 +103,8 @@ int	chech_map_help(char **map)
 			j++;
 		}
 		if (!map[i + 1] && only_ones(map[i]))
-			log_state(ft_strjoin(ft_strjoin("Unclosed wall Line ", ft_strjoin(ft_itoa(i), " ->")), map[i]), 0);
+			log_state(ft_strjoin(ft_strjoin("Unclosed wall Line ",
+						ft_strjoin(ft_itoa(i), " ->")), map[i]), 0);
 		i++;
 	}
 	if (!count)
@@ -120,7 +121,8 @@ int	check_map(t_cub *cub)
 	if (!map)
 		return (log_state("Thers is no map", 0), 1);
 	if (only_ones(map[0]))
-		log_state(ft_strjoin(ft_strjoin("Unclosed wall Line ", ft_strjoin(ft_itoa(0), " ->")), map[0]), 0);
+		log_state(ft_strjoin(ft_strjoin("Unclosed wall Line ",
+					ft_strjoin(ft_itoa(0), " ->")), map[0]), 0);
 	if (chech_map_help(map))
 		return (1);
 	return (check_walls(map));
